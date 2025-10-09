8dff6836adbf9a22952a498f180c58d214404053
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2025-10-09 08:26:36 +0200

    fix(prefer-namespace-import): account for type imports (#40)

--- src/rules/prefer-namespace-import.ts
+++ src/rules/prefer-namespace-import.ts
@@ -1,6 +1,15 @@
+import type { TSESTree } from '@typescript-eslint/utils';
 import { AST_NODE_TYPES, ESLintUtils } from '@typescript-eslint/utils';
 
 import { getRuleURL } from '../meta.js';
+
+function hasNamespaceImport(node: TSESTree.ImportDeclaration): boolean {
+  const { specifiers } = node;
+  return (
+    specifiers.length === 1 &&
+    specifiers[0].type === AST_NODE_TYPES.ImportNamespaceSpecifier
+  );
+}
 
 export const preferNamespaceImport = ESLintUtils.RuleCreator(getRuleURL)({
   name: 'prefer-namespace-import',
@@ -12,39 +21,77 @@ export const preferNamespaceImport = ESLintUtils.RuleCreator(getRuleURL)({
     },
     fixable: 'code',
     messages: {
-      useNamespace:
-        "Import zod as a namespace import (import * as z from 'zod').",
+      removeDuplicate: 'Zod is already imported via namespace import',
+      useNamespace: 'Import zod with a namespace import',
     },
     schema: [],
   },
   defaultOptions: [],
   create(context) {
+    const zodImports: Array<TSESTree.ImportDeclaration> = [];
+    let hasOnlyTypeImports = true;
+
     return {
       ImportDeclaration(node): void {
-        if (
-          node.source.value !== 'zod' &&
-          !node.source.value.startsWith('zod/')
-        ) {
+        const { source, importKind } = node;
+
+        if (source.value !== 'zod' && !source.value.startsWith('zod/')) {
           return;
         }
 
-        // Already namespace import: import * as z from "zod"
-        const hasNamespace = node.specifiers.some(
-          (s) => s.type === AST_NODE_TYPES.ImportNamespaceSpecifier,
-        );
+        if (hasOnlyTypeImports && importKind === 'value') {
+          hasOnlyTypeImports = false;
+        }
 
-        if (!hasNamespace) {
+        zodImports.push(node);
+      },
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      'Program:exit': (): void => {
+        if (zodImports.length === 0) {
+          return;
+        }
+
+        const [firstImportNode, ...othersImportNodes] = zodImports;
+
+        let isFirstImportValid = false;
+        // pick the first import and check if is valid
+        if (hasNamespaceImport(firstImportNode)) {
+          if (hasOnlyTypeImports) {
+            if (firstImportNode.importKind === 'type') {
+              isFirstImportValid = true;
+            }
+          } else {
+            isFirstImportValid = true;
+          }
+        }
+
+        if (!isFirstImportValid) {
           context.report({
-            node,
+            node: firstImportNode,
             messageId: 'useNamespace',
             fix(fixer) {
-              const [firstSpecifier] = node.specifiers;
+              const [firstSpecifier] = firstImportNode.specifiers;
               const localName = firstSpecifier.local.name;
+
+              const importTexts = ['import'];
+              if (hasOnlyTypeImports) {
+                importTexts.push('type');
+              }
+
               return fixer.replaceText(
-                node,
-                `import * as ${localName} from '${node.source.value}';`,
+                firstImportNode,
+                `${importTexts.join(' ')} * as ${localName} from ${firstImportNode.source.raw};`,
               );
             },
+          });
+        }
+
+        // Remove other imports nodes
+        for (const importNode of othersImportNodes) {
+          context.report({
+            node: importNode,
+            messageId: 'removeDuplicate',
+            fix: (fixer) => fixer.removeRange(importNode.range),
           });
         }
       },
