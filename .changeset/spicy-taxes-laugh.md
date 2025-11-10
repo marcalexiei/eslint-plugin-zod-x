7b28d3a28ae91ae0a3ff55f3ad530d2691cecf2a
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2025-11-10 18:01:58 +0100

    fix(prefer-namespace-import): treat each source as group to allow using multiple versions (#95)

---
+++ .changeset/spicy-taxes-laugh.md
@@ -0,0 +1,5 @@
+---
+'eslint-plugin-zod-x': patch
+---
+
+fix(prefer-namespace-import): treat each source as standalone group to allow using multiple versions
--- src/rules/prefer-namespace-import.ts
+++ src/rules/prefer-namespace-import.ts
@@ -21,15 +21,18 @@ export const preferNamespaceImport = ESLintUtils.RuleCreator(getRuleURL)({
     },
     fixable: 'code',
     messages: {
-      removeDuplicate: 'Zod is already imported via namespace import',
       useNamespace: 'Import zod with a namespace import',
+      removeDuplicate: 'Zod is already imported via namespace import',
     },
     schema: [],
   },
   defaultOptions: [],
   create(context) {
-    const zodImports: Array<TSESTree.ImportDeclaration> = [];
-    let hasOnlyTypeImports = true;
+    interface ImportGroupData {
+      hasOnlyTypeImports: boolean;
+      nodes: Array<TSESTree.ImportDeclaration>;
+    }
+    const importGroups: Record<string, ImportGroupData> = {};
 
     return {
       ImportDeclaration(node): void {
@@ -39,60 +42,71 @@ export const preferNamespaceImport = ESLintUtils.RuleCreator(getRuleURL)({
           return;
         }
 
-        if (hasOnlyTypeImports && importKind === 'value') {
-          hasOnlyTypeImports = false;
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        if (!importGroups[source.value]) {
+          importGroups[source.value] = {
+            hasOnlyTypeImports: true,
+            nodes: [],
+          };
         }
 
-        zodImports.push(node);
+        if (
+          importGroups[source.value].hasOnlyTypeImports &&
+          importKind === 'value'
+        ) {
+          importGroups[source.value].hasOnlyTypeImports = false;
+        }
+
+        importGroups[source.value].nodes.push(node);
       },
       // eslint-disable-next-line @typescript-eslint/naming-convention
       'Program:exit': (): void => {
-        if (zodImports.length === 0) {
-          return;
-        }
+        for (const [, { hasOnlyTypeImports, nodes }] of Object.entries(
+          importGroups,
+        )) {
+          const [firstImportNode, ...othersImportNodes] = nodes;
 
-        const [firstImportNode, ...othersImportNodes] = zodImports;
-
-        let isFirstImportValid = false;
-        // pick the first import and check if is valid
-        if (hasNamespaceImport(firstImportNode)) {
-          if (hasOnlyTypeImports) {
-            if (firstImportNode.importKind === 'type') {
+          let isFirstImportValid = false;
+          // pick the first import and check if is valid
+          if (hasNamespaceImport(firstImportNode)) {
+            if (hasOnlyTypeImports) {
+              if (firstImportNode.importKind === 'type') {
+                isFirstImportValid = true;
+              }
+            } else {
               isFirstImportValid = true;
             }
-          } else {
-            isFirstImportValid = true;
           }
-        }
 
-        if (!isFirstImportValid) {
-          context.report({
-            node: firstImportNode,
-            messageId: 'useNamespace',
-            fix(fixer) {
-              const [firstSpecifier] = firstImportNode.specifiers;
-              const localName = firstSpecifier.local.name;
+          if (!isFirstImportValid) {
+            context.report({
+              node: firstImportNode,
+              messageId: 'useNamespace',
+              fix(fixer) {
+                const [firstSpecifier] = firstImportNode.specifiers;
+                const localName = firstSpecifier.local.name;
 
-              const importTexts = ['import'];
-              if (hasOnlyTypeImports) {
-                importTexts.push('type');
-              }
+                const importTexts = ['import'];
+                if (hasOnlyTypeImports) {
+                  importTexts.push('type');
+                }
 
-              return fixer.replaceText(
-                firstImportNode,
-                `${importTexts.join(' ')} * as ${localName} from ${firstImportNode.source.raw};`,
-              );
-            },
-          });
-        }
+                return fixer.replaceText(
+                  firstImportNode,
+                  `${importTexts.join(' ')} * as ${localName} from ${firstImportNode.source.raw};`,
+                );
+              },
+            });
+          }
 
-        // Remove other imports nodes
-        for (const importNode of othersImportNodes) {
-          context.report({
-            node: importNode,
-            messageId: 'removeDuplicate',
-            fix: (fixer) => fixer.removeRange(importNode.range),
-          });
+          // Remove other imports nodes
+          for (const importNode of othersImportNodes) {
+            context.report({
+              node: importNode,
+              messageId: 'removeDuplicate',
+              fix: (fixer) => fixer.removeRange(importNode.range),
+            });
+          }
         }
       },
     };
