940c4fdd5316daf654bdcd9d07bea0da86f398d8
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2026-09-04 23:15:32 +0200

    refactor!: match record-key transforms through the constraint collector (#421)

--- plugins/eslint-plugin-zod/src/rules/no-transform-in-record-key.ts
+++ plugins/eslint-plugin-zod/src/rules/no-transform-in-record-key.ts
@@ -1,6 +1,5 @@
 import { ZOD_MUTATING_CHECK_NAMES, zodImportScope } from '@eslint-zod/utils';
 import { buildNoTransformInRecordKeyCreate } from '@eslint-zod/utils/rule-builders/no-transform-in-record-key';
-import { AST_NODE_TYPES } from '@typescript-eslint/utils';
 
 import { createZodPluginRule } from '../utils/create-plugin-rule.js';
 
@@ -21,17 +20,5 @@ export const noTransformInRecordKey = createZodPluginRule({
     schema: [],
   },
   defaultOptions: [],
-  create: buildNoTransformInRecordKeyCreate(zodImportScope, {
-    findTransformNode(keySchema, { collectZodChainMethods }) {
-      if (keySchema.type !== AST_NODE_TYPES.CallExpression) {
-        return null;
-      }
-
-      const hasTransform = collectZodChainMethods(keySchema).some((method) =>
-        TRANSFORM_METHODS.includes(method.name),
-      );
-
-      return hasTransform ? keySchema : null;
-    },
-  }),
+  create: buildNoTransformInRecordKeyCreate(zodImportScope, TRANSFORM_METHODS),
 });
